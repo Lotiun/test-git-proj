b6ea7acf082168ee11f977d686c7eff72f149f09
nickgiann <nickgiann@hotmail.com>
2024-12-18 15:01:20 +0200

    Correct initial commit

--- src/gr/aueb/dt/ch1/HelloWorld.java
+++ src/gr/aueb/dt/ch1/HelloWorld.java
@@ -3,6 +3,6 @@ package gr.aueb.dt.ch1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git!");
     }
 }
